5c7d9a022f24d778d8fa0da187fc494a2a21b52b
拿铁自然卷 <2895358020@qq.com>
2018-11-27 02:03:45 +0800

    项目架构

--- js/index.js
+++ js/index.js
@@ -1,7 +1,28 @@
 $(function () {
-
+    var MMB = new mmb();
+    MMB.getindexmenu();
+    MMB.getmoneyctrl();
 })
 var mmb = function () {};
 mmb.prototype = {
-	
+    /* 获取首页菜单栏信息 */
+    getindexmenu: function () {
+        $.ajax({
+            url: 'http://localhost:9090/api/getindexmenu',
+            success: function (obj) {
+                var html = template('menuTpl', obj);
+                $('.menu ul').html(html);
+            }
+        })
+    },
+    /* 获取优惠商品 */
+    getmoneyctrl: function () {
+        $.ajax({
+            url: 'http://localhost:9090/api/getmoneyctrl',
+            success: function (obj) {
+                var html = template('moneyctrlTpl', obj);
+                $('.productBox').html(html);
+            }
+        })
+    }
 }
