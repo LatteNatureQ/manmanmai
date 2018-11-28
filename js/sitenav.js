d7be2c0528d99bbbc33c18597663ae82731af88c
imsuchafool <2857541377@qq.com>
2018-11-28 10:39:33 +0800

    柴琛完成修改(未污染base.less文件)

--- js/sitenav.js
+++ js/sitenav.js
@@ -18,6 +18,7 @@ MMB.prototype = {
             success: function (result) { 
                 console.log(result);
                 var html = template('sitenavTpl',result);
+                console.log(html);
                 $('.sitenavList').html(html);
             }
         })
