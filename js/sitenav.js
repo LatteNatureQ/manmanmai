529eaa573e4d8038a95856749886c4cb1e6e7754
imsuchafool <2857541377@qq.com>
2018-11-28 10:32:09 +0800

    柴琛完成项目(未污染base.less)

---
+++ js/sitenav.js
@@ -0,0 +1,27 @@
+$(function () {
+    var mmb = new MMB();
+    mmb.setScroll();
+    mmb.querySitenav();
+})
+var MMB = function () {};
+MMB.prototype = {
+    /* 初始化scroll控件 */
+    setScroll: function () {
+        mui('.mui-scroll-wrapper').scroll({
+            deceleration: 0.0005
+        });
+    },
+
+    querySitenav: function () { 
+        $.ajax({
+            url: 'http://localhost:9090/api/getsitenav',
+            success: function (result) { 
+                console.log(result);
+                var html = template('sitenavTpl',result);
+                $('.sitenavList').html(html);
+            }
+        })
+     }
+
+
+}
