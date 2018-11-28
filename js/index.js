1eb32f715b6d7be0082ebec0e875d76534e93356
拿铁自然卷 <2895358020@qq.com>
2018-11-28 23:19:47 +0800

    项目架构

--- js/index.js
+++ js/index.js
@@ -1,28 +1,59 @@
 $(function () {
     var MMB = new mmb();
+    MMB.hide();
     MMB.getindexmenu();
     MMB.getmoneyctrl();
+    MMB.close();
+    MMB.productList();
 })
 var mmb = function () {};
 mmb.prototype = {
     /* 获取首页菜单栏信息 */
+    baseURL: 'http://localhost:9090',
     getindexmenu: function () {
+        var that = this;
         $.ajax({
-            url: 'http://localhost:9090/api/getindexmenu',
+            url: that.baseURL + '/api/getindexmenu',
             success: function (obj) {
                 var html = template('menuTpl', obj);
-                $('.menu ul').html(html);
+                $('.menu ul').append(html);
+                $('.li7 a').attr('href', 'javascript:;')
+                $('.li7 a').on('tap', function () {
+                    console.log(111);
+                })
             }
         })
     },
     /* 获取优惠商品 */
     getmoneyctrl: function () {
+        var that = this;
         $.ajax({
-            url: 'http://localhost:9090/api/getmoneyctrl',
+            url: that.baseURL + '/api/getmoneyctrl',
             success: function (obj) {
+                console.log(obj);
                 var html = template('moneyctrlTpl', obj);
                 $('.productBox').html(html);
             }
+        });
+    },
+    close: function () {
+        $('.close').on('tap', function () {
+            localStorage.setItem('key', 'show');
+            console.log(localStorage.getItem('key'));
+            $('#iframe-wrap').remove();
+            $('.menu li').addClass('animated tada')
+        })
+    },
+    hide: function () {
+        if (localStorage.getItem('key')) {
+            $('#iframe-wrap').remove();
+        }
+    },
+    /* 跳转到商品详情页 */
+    productList: function () {
+        $('#main').on('tap', '.productList', function () {
+            var id = $(this).data('id');
+            location = 'productid.html?productid=' + id;
         })
     }
 }
